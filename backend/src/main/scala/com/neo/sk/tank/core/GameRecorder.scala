d5cdc1a71eaee44ac6a932ff115b521a274d118d
hongruying <hongruying@neotel.com.cn>
2018-08-15 00:08:13 +0800

    debug

---
+++ backend/src/main/scala/com/neo/sk/tank/core/GameRecorder.scala
@@ -0,0 +1,14 @@
+package com.neo.sk.tank.core
+
+
+/**
+  * Created by hongruying on 2018/8/14
+  * 采用essf工具进行记录
+  * 每次玩家进入游戏，死亡效果。
+  * 主要记录每帧的用户事件，环境事件，还有游戏状态
+  */
+object GameRecorder {
+
+
+
+}
