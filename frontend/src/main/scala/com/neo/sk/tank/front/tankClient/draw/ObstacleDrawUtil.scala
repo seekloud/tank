e0718f9b66a57983181dc509224cd2df815381b3
hongruying <hongruying@neotel.com.cn>
2018-09-11 12:32:52 +0800

    障碍物碰撞体积减小，碰到障碍物还能移动，点击立即开始，道具消失机制和动画

--- frontend/src/main/scala/com/neo/sk/tank/front/tankClient/draw/ObstacleDrawUtil.scala
+++ frontend/src/main/scala/com/neo/sk/tank/front/tankClient/draw/ObstacleDrawUtil.scala
@@ -2,7 +2,7 @@ package com.neo.sk.tank.front.tankClient.draw
 
 import com.neo.sk.tank.front.common.Routes
 import com.neo.sk.tank.front.tankClient.GameContainerClientImpl
-import com.neo.sk.tank.shared.`object`.Obstacle
+import com.neo.sk.tank.shared.`object`.{AirDropBox, Obstacle}
 import com.neo.sk.tank.shared.model.Constants.ObstacleType
 import com.neo.sk.tank.shared.model.Point
 import org.scalajs.dom
@@ -63,6 +63,7 @@ trait ObstacleDrawUtil{ this:GameContainerClientImpl =>
   protected def drawObstacles(offset:Point,view:Point) = {
     obstacleMap.values.foreach{ obstacle =>
       if((obstacle.getPosition + offset).in(view,Point(obstacle.getWidth,obstacle.getHeight))) {
+        val isAttacked: Boolean = obstacleAttackedAnimationMap.get(obstacle.oId).nonEmpty
         val color = (obstacle.obstacleType, obstacleAttackedAnimationMap.get(obstacle.oId).nonEmpty) match {
           case (ObstacleType.airDropBox, true) =>
             if (obstacleAttackedAnimationMap(obstacle.oId) <= 0) obstacleAttackedAnimationMap.remove(obstacle.oId)
@@ -79,35 +80,50 @@ trait ObstacleDrawUtil{ this:GameContainerClientImpl =>
             s"rgba(139 ,105, 105,1)"
         }
         if(obstacle.obstacleType == ObstacleType.airDropBox){
-          if (obstacle.bloodPercent() > 0.99999999){
-            val p = obstacle.getPosition + offset - Point(obstacle.getWidth / 2, obstacle.getHeight / 2)
+          val p = obstacle.getPosition + offset - Point(obstacle.getWidth / 2, obstacle.getHeight / 2)
+          if (isAttacked){
+            ctx.globalAlpha = 0.5
+            ctx.drawImage(airBoxImg.asInstanceOf[HTMLElement], p.x * canvasUnit, p.y * canvasUnit,
+              obstacle.getWidth * canvasUnit, obstacle.getHeight * canvasUnit)
+            ctx.globalAlpha = 1
+          } else {
             ctx.drawImage(airBoxImg.asInstanceOf[HTMLElement], p.x * canvasUnit, p.y * canvasUnit,
               obstacle.getWidth * canvasUnit, obstacle.getHeight * canvasUnit)
           }
-          else{
-            val p1 = obstacle.getPosition + offset - Point(obstacle.getWidth / 2, obstacle.getHeight / 2)
-            val p2 = obstacle.getPosition + offset - Point(obstacle.getWidth / 2, obstacle.getHeight / 2)+Point(0,(1-obstacle.bloodPercent())*obstacle.getHeight)
-            ctx.globalAlpha = 0.3
 
-            val cacheCanvas = dom.document.createElement("canvas").asInstanceOf[html.Canvas]
-            val ctxCache = cacheCanvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
-            cacheCanvas.width = (obstacle.getWidth * canvasUnit).toInt
-            cacheCanvas.height = (obstacle.getHeight * canvasUnit).toInt
-            ctxCache.drawImage(airBoxImg,0,0,obstacle.getWidth * canvasUnit,obstacle.getHeight * canvasUnit)
+//          if(obstacle.bloodPercent() < 0.99999999){
+//            drawLine(p.x * canvasUnit, (p.y - 2) * canvasUnit, 10, obstacle.getWidth * canvasUnit, "#4D4D4D")
+//            drawLine(p.x * canvasUnit, (p.y - 2) * canvasUnit, 5, obstacle.getWidth * canvasUnit * obstacle.bloodPercent(), "#98FB98")
+//          }
 
 
-
-            ctx.drawImage(cacheCanvas, 0, 0, obstacle.getWidth * canvasUnit, obstacle.getHeight *(1-obstacle.bloodPercent())* canvasUnit,
-              p1.x * canvasUnit, p1.y * canvasUnit,
-              obstacle.getWidth * canvasUnit, obstacle.getHeight *(1-obstacle.bloodPercent())* canvasUnit)
-            ctx.globalAlpha = 1
-            ctx.drawImage(cacheCanvas, 0,(1-obstacle.bloodPercent())*obstacle.getHeight * canvasUnit ,
-              obstacle.getWidth * canvasUnit, obstacle.getHeight*obstacle.bloodPercent() * canvasUnit,
-              p2.x * canvasUnit, p2.y * canvasUnit,
-              obstacle.getWidth * canvasUnit,obstacle.getHeight *obstacle.bloodPercent()* canvasUnit)
-
-
-          }
+//          if (obstacle.bloodPercent() > 0.99999999){
+//            ctx.drawImage(airBoxImg.asInstanceOf[HTMLElement], p.x * canvasUnit, p.y * canvasUnit,
+//              obstacle.getWidth * canvasUnit, obstacle.getHeight * canvasUnit)
+//          }
+//          else{
+//            val p1 = obstacle.getPosition + offset - Point(obstacle.getWidth / 2, obstacle.getHeight / 2)
+//            val p2 = obstacle.getPosition + offset - Point(obstacle.getWidth / 2, obstacle.getHeight / 2)+Point(0,(1-obstacle.bloodPercent())*obstacle.getHeight)
+//            ctx.globalAlpha = 0.3
+//
+//            val cacheCanvas = dom.document.createElement("canvas").asInstanceOf[html.Canvas]
+//            val ctxCache = cacheCanvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
+//            cacheCanvas.width = (obstacle.getWidth * canvasUnit).toInt
+//            cacheCanvas.height = (obstacle.getHeight * canvasUnit).toInt
+//            ctxCache.drawImage(airBoxImg,0,0,obstacle.getWidth * canvasUnit,obstacle.getHeight * canvasUnit)
+//
+//
+//
+//            ctx.drawImage(cacheCanvas, 0, 0, obstacle.getWidth * canvasUnit, obstacle.getHeight *(1-obstacle.bloodPercent())* canvasUnit,
+//              p1.x * canvasUnit, p1.y * canvasUnit,
+//              obstacle.getWidth * canvasUnit, obstacle.getHeight *(1-obstacle.bloodPercent())* canvasUnit)
+//            ctx.globalAlpha = 1
+//            ctx.drawImage(cacheCanvas, 0,(1-obstacle.bloodPercent())*obstacle.getHeight * canvasUnit ,
+//              obstacle.getWidth * canvasUnit, obstacle.getHeight*obstacle.bloodPercent() * canvasUnit,
+//              p2.x * canvasUnit, p2.y * canvasUnit,
+//              obstacle.getWidth * canvasUnit,obstacle.getHeight *obstacle.bloodPercent()* canvasUnit)
+//
+//          }
 
         }else{
           if (obstacle.bloodPercent() > 0.9999999) {
@@ -122,6 +138,31 @@ trait ObstacleDrawUtil{ this:GameContainerClientImpl =>
 
       }
     }
+  }
+
+
+  def drawObstacleBloodSlider(offset:Point) = {
+    obstacleMap.values.filter(_.isInstanceOf[AirDropBox]).foreach{ obstacle =>
+      if(obstacle.bloodPercent() < 0.99999999){
+        val p = obstacle.getPosition + offset - Point(obstacle.getWidth / 2, obstacle.getHeight / 2)
+        drawLine(p.x * canvasUnit, (p.y - 2) * canvasUnit, 10, obstacle.getWidth * canvasUnit, "#4D4D4D")
+        drawLine(p.x * canvasUnit, (p.y - 2) * canvasUnit, 5, obstacle.getWidth * canvasUnit * obstacle.bloodPercent(), "#98FB98")
+      }
+    }
+  }
+
+  //画血量条
+  private def drawLine(startX: Float, startY: Float, lineWidth:Float, lineLen:Float, color:String) = {
+    ctx.save()
+    ctx.lineWidth = lineWidth
+    ctx.lineCap = "round"
+    ctx.strokeStyle = color
+    ctx.beginPath()
+    ctx.moveTo(startX, startY)
+    ctx.lineTo(startX + lineLen, startY)
+    ctx.stroke()
+    ctx.closePath()
+    ctx.restore()
   }
 
 
