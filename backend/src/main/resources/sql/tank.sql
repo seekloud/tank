deec3b458af54f880116c14055aa9380ef2f1395
hongruying <hongruying@neotel.com.cn>
2018-10-12 16:40:05 +0800

    sql

--- backend/src/main/resources/sql/tank.sql
+++ backend/src/main/resources/sql/tank.sql
@@ -1,6 +1,19 @@
 --定义数据表
 
 --
-CREATE TABLE record(
 
-)
+
+CREATE TABLE game_record(
+record_id SERIAL8 PRIMARY KEY NOT NULL ,
+room_id BIGINT NOT NULL ,
+start_time BIGINT NOT NULL ,
+end_time BIGINT NOT NULL ,
+file_path TEXT NOT NULL
+);
+
+
+create TABLE user_record_map(
+user_id BIGINT NOT NULL ,
+record_id BIGINT NOT NULL ,
+room_id BIGINT NOT NULL
+);
