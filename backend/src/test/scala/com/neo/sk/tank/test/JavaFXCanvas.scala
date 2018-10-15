66ec80f1655d61cc87b60bf247de5093f3d154a9
hongruying <hongruying@neotel.com.cn>
2018-10-15 16:09:31 +0800

    scalafx test

--- backend/src/test/scala/com/neo/sk/tank/test/JavaFXCanvas.scala
+++ backend/src/test/scala/com/neo/sk/tank/test/JavaFXCanvas.scala
@@ -3,6 +3,9 @@ package com.neo.sk.tank.test
 
 
 
+import javafx.event.EventHandler
+import javafx.scene.input
+import javafx.scene.shape.StrokeLineCap
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.{Scene, SnapshotParameters}
@@ -12,6 +15,7 @@ import scalafx.scene.paint.Color._
 import scalafx.scene.paint.{LinearGradient, Stops}
 import scalafx.scene.text.Text
 import scalafx.scene.canvas.Canvas
+import scalafx.scene.input.{KeyEvent, MouseEvent}
 import scalafx.scene.paint.Color
 import scalafx.scene.shape.ArcType
 
@@ -59,7 +63,7 @@ object JavaFXCanvas extends JFXApp{
   }
 
 
-  val canvas = new Canvas(300, 300)
+  val canvas = new Canvas(1000, 900)
   val gc = canvas.graphicsContext2D
 
   gc.fill = Color.Green
@@ -80,14 +84,106 @@ object JavaFXCanvas extends JFXApp{
   gc.fillPolygon(Seq((10.0, 210), (40, 210), (10, 240), (40, 240)))
   gc.strokePolygon(Seq((60.0, 210), (90, 210), (60, 240), (90, 240)))
   gc.strokePolyline(Seq((110.0, 210), (140, 210), (110, 240), (140, 240)))
+  gc.setFill(Color.Red)
+  gc.setStroke(Color.Red)
+  gc.setLineWidth(2)
+  gc.beginPath()
+  gc.moveTo(100,10)
+  gc.lineTo(100,100)
+  gc.lineTo(700,100)
+  gc.lineTo(700,10)
+  gc.lineTo(100,10)
+  gc.closePath()
+  gc.stroke()
+  gc.fill()
+
+  gc.setLineCap(StrokeLineCap.SQUARE)
+  gc.setLineWidth(20)
+  gc.beginPath()
+  gc.moveTo(100,300)
+  gc.lineTo(700,600)
+  gc.closePath()
+  gc.stroke()
+
+  gc.setLineCap(StrokeLineCap.BUTT)
+  gc.setStroke(Color.Black)
+  gc.setLineWidth(20)
+  gc.beginPath()
+  gc.moveTo(100,300)
+  gc.lineTo(700,600)
+  gc.closePath()
+  gc.stroke()
+  //test cacheCanvas
+
+  val canvasCache = new Canvas(1000, 900)
+  val gcCache = canvasCache.graphicsContext2D
+  gcCache.fill = Color.Blue
+  gcCache.stroke = Color.Blue
+  gcCache.fillRect(0,0,1000,900)
+
+
+
+  gc.drawImage(canvasCache.snapshot(new SnapshotParameters(), null),0,0,1000,900)
+
+  val handler:MouseEvent => Unit = { e: MouseEvent =>
+    e.eventType match {
+      case MouseEvent.MouseClicked => println(s"mouse click=${(e.x,e.y)}")
+      case _ =>
+        if(e.eventType == MouseEvent.MouseMoved){
+          println(s"mouse move =${(e.x,e.y)}")
+        }
+        println(s"mouse unknow type=${e.eventType}")
+    }
+  }
+  val keyHandler : KeyEvent => Unit = { e:KeyEvent =>
+    e.eventType match {
+      case KeyEvent.KeyPressed => println(s"key code=${e.code} press")
+      case KeyEvent.KeyReleased => println(s"key code=${e.code} release")
+      case _ => println(s"key ssss")
+
+    }
+
+  }
+
+  import scalafx.Includes._
+  canvas.requestFocus()
+  canvas.handleEvent(MouseEvent.Any) (handler)
+//  canvas.handleEvent(KeyEvent.Any) (keyHandler)
+
+
+
+
+
+//  canvas.addEventHandler(MouseEvent.Any, {(e:MouseEvent) => println(s"Some mouse event handled,${}")})
+//  canvas.handleEvent(MouseEvent.Any){ (me:MouseEvent) => {println("Some mouse event handled")}}
+
+
   val img = canvas.snapshot(new SnapshotParameters(), null)
+  val myScene = new Scene {
+    content = canvas
+  }
+
+  myScene.onKeyPressed_=(new EventHandler[javafx.scene.input.KeyEvent]{
+    override def handle(event: input.KeyEvent): Unit = {
+      println(s"event type=${event.getEventType.getName}, code=${event.getCode}")
+    }
+  })
+
+  myScene.onKeyReleased_=(new EventHandler[javafx.scene.input.KeyEvent]{
+    override def handle(event: input.KeyEvent): Unit = {
+      println(s"event type=${event.getEventType.getName}, code=${event.getCode}")
+    }
+  })
+
   println(img.getPixelReader.getPixelFormat().getType)
   val canvasTestStage = new PrimaryStage {
     title = "Drawing Operations Test"
-    scene = new Scene {
-      content = canvas
-    }
+    width = canvas.width.toDouble
+    height = canvas.height.toDouble
+    scene = myScene
   }
+
+  //不支持画线的帽
 
   stage = canvasTestStage
 }
