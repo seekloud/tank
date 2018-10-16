caccde4425cc1f6e1d9d9303955850fef46f8a07
hongruying <hongruying@neotel.com.cn>
2018-10-16 18:04:09 +0800

    debug

--- backend/src/main/scala/com/neo/sk/tank/core/RoomActor.scala
+++ backend/src/main/scala/com/neo/sk/tank/core/RoomActor.scala
@@ -129,11 +129,14 @@ object RoomActor {
           val startTime = System.currentTimeMillis()
 
 
-          val record = gameContainer.getGameEventAndSnapshot()
-          if(AppSettings.gameRecordIsWork){
-            getGameRecorder(ctx,gameContainer) ! GameRecorder.GameRecord(record)
-          }
+          val snapshotOpt = gameContainer.getCurSnapshot()
+
           gameContainer.update()
+
+          val gameEvents = gameContainer.getLastGameEvent()
+          if(AppSettings.gameRecordIsWork){
+            getGameRecorder(ctx,gameContainer) ! GameRecorder.GameRecord(gameEvents, snapshotOpt)
+          }
 
 
 
