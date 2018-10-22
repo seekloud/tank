902984975b40ddcd0ccce27ec095ebdcbaf8ae4c
hongruying <hongruying@neotel.com.cn>
2018-10-22 11:37:18 +0800

    debug

--- backend/src/main/scala/com/neo/sk/tank/core/UserActor.scala
+++ backend/src/main/scala/com/neo/sk/tank/core/UserActor.scala
@@ -47,7 +47,7 @@ object UserActor {
 
   case class DispatchMsg(msg:TankGameEvent.WsMsgSource) extends Command
 
-  case object StartGame extends Command
+  case class StartGame(roomId:Option[Long]) extends Command
   case class JoinRoom(uid:Long,tankIdOpt:Option[Int],name:String,userActor:ActorRef[UserActor.Command]) extends Command with RoomManager.Command
 
   case class JoinRoomSuccess(tank:TankServerImpl,config:TankGameConfigImpl,uId:Long,roomActor: ActorRef[RoomActor.Command]) extends Command with RoomManager.Command
@@ -165,7 +165,7 @@ object UserActor {
   ): Behavior[Command] =
     Behaviors.receive[Command] { (ctx, msg) =>
       msg match {
-        case StartGame =>
+        case StartGame(roomIdOpt) =>
           /**换成给roomManager发消息,告知uId,name
             * 还要给userActor发送回带roomId的数据
             * */
