e39172ec9ce6997e65dd6d868b85fd880e76680c
曹慎凯 <caoshenkai@neotel.com.cn>
2018-10-26 17:44:19 +0800

    尝试client  WebSocket

--- client/src/main/scala/com/neo/sk/tank/actor/PlayGameActor.scala
+++ client/src/main/scala/com/neo/sk/tank/actor/PlayGameActor.scala
@@ -2,13 +2,23 @@ package com.neo.sk.tank.actor
 
 import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.{Behaviors, StashBuffer, TimerScheduler}
+import akka.http.javadsl.model.ws.WebSocketRequest
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.ws.{BinaryMessage, Message, TextMessage}
+import akka.stream.scaladsl.Flow
+import akka.stream.typed.scaladsl.ActorSink
+import com.neo.sk.tank.shared.protocol.TankGameEvent.{CompleteMsgServer, FailMsgServer, WsMsgSource}
+import org.seekloud.byteobject.ByteObject.bytesDecode
+import org.seekloud.byteobject.MiddleBufferInJvm
+import org.slf4j.LoggerFactory
 /**
   * Created by hongruying on 2018/10/23
   * 连接游戏服务器的websocket Actor
   */
 object PlayGameActor {
+  private val log = LoggerFactory.getLogger(this.getClass)
   sealed trait Command
+  final case class ConnectGame(name:String) extends Command
 
   /**进入游戏连接参数*/
   def create()={
@@ -26,6 +36,11 @@ object PlayGameActor {
     timer:TimerScheduler[Command])={
     Behaviors.receive[Command]{(ctx,msg)=>
       msg match {
+        case msg:ConnectGame=>
+          val url=getWebSocketUri(msg.name)
+          val webSocketFlow=Http().webSocketClientFlow(WebSocketRequest(url))
+
+          Behaviors.same
         case x=>
           Behaviors.unhandled
       }
@@ -40,5 +55,28 @@ object PlayGameActor {
           Behaviors.unhandled
       }
     }
+  }
+
+  def getSink =
+    Flow[Message].collect {
+      case TextMessage.Strict(msg) =>
+        log.debug(s"msg from webSocket: $msg")
+
+      case BinaryMessage.Strict(bMsg) =>
+        //decode process.
+        val buffer = new MiddleBufferInJvm(bMsg.asByteBuffer)
+        val msg =
+          bytesDecode[](buffer) match {
+            case Right(v) => v
+            case Left(e) =>
+              println(s"decode error: ${e.message}")
+          }
+        msg
+    }.to(ActorSink.actorRef[WsMsgSource](actor, CompleteMsgServer, FailMsgServer))
+
+  def getWebSocketUri(name: String): String = {
+    val wsProtocol = "ws"
+    val host ="localhost:30369"
+    s"$wsProtocol://$host/tank/game/join?name=$name"
   }
 }
