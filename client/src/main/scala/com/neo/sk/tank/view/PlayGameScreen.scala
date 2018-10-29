5562e9b0d61160e60461dcdcbab2aa7143d54a04
benyafang <benyafang@neotel.com.cn>
2018-10-29 15:30:26 +0800

    client text

--- client/src/main/scala/com/neo/sk/tank/view/PlayGameScreen.scala
+++ client/src/main/scala/com/neo/sk/tank/view/PlayGameScreen.scala
@@ -3,13 +3,14 @@ package com.neo.sk.tank.view
 import com.neo.sk.tank.common.Context
 import javafx.scene.Scene
 import javafx.scene.canvas.GraphicsContext
+import javafx.scene.layout.BorderPane
 
 /**
   * Created by hongruying on 2018/10/23
   * 玩游戏的view
   *
   */
-class PlayGameScreen(context: Context) {
+class PlayGameScreen(context: Context){
 
 
 
