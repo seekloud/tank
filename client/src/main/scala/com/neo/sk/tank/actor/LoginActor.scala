3819c2b63e13112e7d9ffeebd402cd6cb840e28b
zuomeng <zuomeng@neotel.com.cn>
2018-10-29 14:47:04 +0800

    login

--- client/src/main/scala/com/neo/sk/tank/actor/LoginActor.scala
+++ client/src/main/scala/com/neo/sk/tank/actor/LoginActor.scala
@@ -1,19 +1,22 @@
 package com.neo.sk.tank.actor
 
-import akka.actor.typed.{ActorRef, Behavior}
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed._
+import akka.actor.typed.scaladsl.{Behaviors, TimerScheduler}
+import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.model.ws.{WebSocketRequest, _}
 import akka.stream.OverflowStrategy
-import akka.stream.scaladsl.{Flow, Keep}
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
 import akka.stream.typed.scaladsl.{ActorSink, ActorSource}
-import akka.util.ByteString
+import akka.util.{ByteString, ByteStringBuilder}
 import com.neo.sk.tank.controller.LoginScreenController
 import com.neo.sk.tank.model._
 import org.seekloud.byteobject.MiddleBufferInJvm
 import org.slf4j.LoggerFactory
 import utils.EsheepClient
 
+import scala.concurrent.Future
+import scala.concurrent.duration._
 import scala.util.{Failure, Success}
 
 /**
@@ -58,15 +61,30 @@ object LoginActor {
   def idle(controller: LoginScreenController): Behavior[Command] = {
     Behaviors.receive[Command]{ (ctx, msg) =>
       msg match {
-        case WSLogin(m) =>
+        case WSLogin(url) =>
           val webSocketFlow = Http().webSocketClientFlow(WebSocketRequest(url))
-          val source = getSource
-          val sink = getSink(gameController)
-          val ((stream, response), _) =
-            source
-              .viaMat(webSocketFlow)(Keep.both)
-              .toMat(sink)(Keep.both)
+          //val source = getSource
+          //val sink = getSink(controller)
+          val ((stream, response), closed) =
+            Source.actorRef(10,OverflowStrategy.dropHead)
+              .viaMat(webSocketFlow)(Keep.both) // keep the materialized Future[WebSocketUpgradeResponse]
+              .toMat(incoming)(Keep.both) // also keep the Future[Done]
               .run()
+
+          val connected = response.flatMap { upgrade =>
+            if (upgrade.response.status == StatusCodes.SwitchingProtocols) {
+              // ctx.schedule(10.seconds, stream, TextMessage.Strict("hello"))
+              //ctx.system.scheduler.schedule(1 seconds,30 minutes,stream,TextMessage.Strict("hello")
+              Future.successful(s"$log connect success.")
+            } else {
+              throw new RuntimeException(s"WSClient connection failed: ${upgrade.response.status}")
+            }
+          } //链接建立时
+          connected.onComplete(i => log.info(i.toString))
+          //					closed.onComplete { i =>
+          //						log.error(s"$logPrefix connection closed!")
+          //					} //链接断开时
+          Behaviors.same
 
 
 
@@ -81,40 +99,20 @@ object LoginActor {
   }
 
 
-  def getSink(actor: ActorRef[WsMsgSource]) =
-    Flow[Message].collect {
-      case TextMessage.Strict(msg) =>
-        log.debug(s"msg from webSocket: $msg")
-        TextMsg(msg)
 
-      case BinaryMessage.Strict(bMsg) =>
-        //decode process.
-        val buffer = new MiddleBufferInJvm(bMsg.asByteBuffer)
-        val msg =
-          bytesDecode[GameMessage](buffer) match {
-            case Right(v) => v
-            case Left(e) =>
-              println(s"decode error: ${e.message}")
-              TextMsg("decode error")
-          }
-        msg
-    }.to(ActorSink.actorRef[WsMsgSource](actor, CompleteMsgServer, FailMsgServer))
 
-  def getSource = ActorSource.actorRef[WsSendMsg](
-    completionMatcher = {
-      case WsSendComplete =>
-    }, failureMatcher = {
-      case WsSendFailed(ex) ⇒ ex
-    },
-    bufferSize = 8,
-    overflowStrategy = OverflowStrategy.fail
-  ).collect {
-    case message: UserAction =>
-      val sendBuffer = new MiddleBufferInJvm(409600)
-      BinaryMessage.Strict(ByteString(
-        message.fillMiddleBuffer(sendBuffer).result()
-      ))
-  }
+  val incoming =
+    Sink.foreach[Message] {
+      case msg: WSLoginInfo =>
+        if(msg.errCode == 0){
+          log.debug(s" ws receive userInfo msg: ${msg}")
+          msg.data
+        }else{
+          log.debug(s" ws receive userInfo error")
+        }
+      case unknown =>
+        log.error(s"wsclient receive unknown message:$unknown")
+    }
 
 
 
