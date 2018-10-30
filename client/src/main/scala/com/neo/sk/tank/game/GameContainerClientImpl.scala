ba9b18866ff01a05ad7a910b2fd1f231460a8826
曹慎凯 <caoshenkai@neotel.com.cn>
2018-10-30 15:09:33 +0800

    merge dev-wmy

--- client/src/main/scala/com/neo/sk/tank/game/GameContainerClientImpl.scala
+++ client/src/main/scala/com/neo/sk/tank/game/GameContainerClientImpl.scala
@@ -1,8 +1,7 @@
 package com.neo.sk.tank.game
 
-
 import com.neo.sk.tank.game.draw._
-import com.neo.sk.tank.shared.`object`.Tank
+import com.neo.sk.tank.shared.`object`.{Tank, TankImpl}
 import com.neo.sk.tank.shared.config.TankGameConfig
 import com.neo.sk.tank.shared.game.GameContainerImpl
 import com.neo.sk.tank.shared.model.Constants.{GameAnimation, PropGenerateType}
@@ -10,6 +9,16 @@ import com.neo.sk.tank.shared.model.Point
 import com.neo.sk.tank.shared.protocol.TankGameEvent
 import javafx.scene.canvas.GraphicsContext
 import com.neo.sk.tank.common.Constants.GameState
+import javafx.geometry.VPos
+import javafx.scene.paint.Color
+import javafx.scene.shape.{StrokeLineCap, StrokeLineJoin}
+import javafx.scene.text.{Font, TextAlignment}
+import javafx.stage.Screen
+import java.util.Timer
+import java.util.TimerTask
+
+import com.neo.sk.tank.App.scheduler
+import concurrent.duration._
 
 import scala.collection.mutable
 
@@ -22,7 +31,7 @@ case class GameContainerClientImpl(
                                     myId:String,
                                     myTankId:Int,
                                     myName:String,
-                                    canvasBoundary:Point,
+                                    canvasSize:Point,
                                     canvasUnit:Int,
                                     setGameState:Int => Unit
                                   ) extends GameContainerImpl(config, myId, myTankId, myName)
@@ -37,6 +46,22 @@ case class GameContainerClientImpl(
   protected val tankAttackedAnimationMap = mutable.HashMap[Int,Int]()
   protected val tankDestroyAnimationMap = mutable.HashMap[Int,Int]() //prop ->
 
+  private var canvasBoundary=canvasSize
+  private var renderTime:Long = 0
+  private var renderTimes = 0
+
+  val timer = new Timer()
+  timer.schedule(new TimerTask {
+    override def run(): Unit = {
+      if(renderTimes != 0){
+        println(s"render page use avg time:${renderTime / renderTimes}ms")
+      }else{
+        println(s"render page use avg time:0 ms")
+      }
+      renderTime = 0
+      renderTimes = 0
+    }
+  }, 0, 5000)
 
   override protected def handleObstacleAttacked(e: TankGameEvent.ObstacleAttacked): Unit = {
     super.handleObstacleAttacked(e)
@@ -73,12 +98,50 @@ case class GameContainerClientImpl(
     * 游戏画面绘制
     * */
   def drawGame(time:Long,networkLatency: Long):Unit = {
+    val offsetTime = math.min(time,config.frameDuration)
+    val bounds = Screen.getPrimary.getVisualBounds
+    val h = bounds.getMaxY.toFloat
+    val w = bounds.getMaxX.toFloat
+    val startTime = System.currentTimeMillis()
+    if(!waitSyncData){
+      ctx.setLineCap(StrokeLineCap.ROUND)
+      ctx.setLineJoin(StrokeLineJoin.ROUND)
+      tankMap.get(myTankId) match {
+        case Some(tank) =>
+          val offset = canvasBoundary / 2 - tank.asInstanceOf[TankImpl].getPosition4Animation(boundary, quadTree, offsetTime)
+          drawBackground(offset)
+          drawObstacles(offset,Point(w,h))
+          drawEnvironment(offset,Point(w,h))
+          drawProps(offset,Point(w,h))
+          drawBullet(offset,offsetTime, Point(w,h))
+          drawTank(offset,offsetTime,Point(w,h))
+          drawObstacleBloodSlider(offset)
+          drawMyTankInfo(tank.asInstanceOf[TankImpl])
+          drawMinimap(tank)
+          drawRank()
+          renderFps(networkLatency)
+          drawKillInformation()
+          drawRoomNumber()
+          drawCurMedicalNum(tank.asInstanceOf[TankImpl])
 
+          if(tank.cavasFrame >=1) {
+            tank.cavasFrame += 1
+          }
+          val endTime = System.currentTimeMillis()
+          renderTimes += 1
+          renderTime += endTime - startTime
+        case None =>
+      }
+    }
   }
 
-
-
-
-
-
+  def drawDeadImg(s:String) = {
+    ctx.setFill(Color.BLACK)
+    ctx.fillRect(0, 0, canvasBoundary.x * canvasUnit, canvasBoundary.y * canvasUnit)
+    ctx.setFill(Color.rgb(250, 250, 250))
+    ctx.setTextAlign(TextAlignment.LEFT)
+    ctx.setTextBaseline(VPos.TOP)
+    ctx.setFont(Font.font("Helvetica", 36))
+    ctx.fillText(s"$s", 150, 180)
+  }
 }
