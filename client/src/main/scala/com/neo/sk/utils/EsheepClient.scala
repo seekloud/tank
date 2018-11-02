9f209592f9aa2ef0503df2d60e6edd79ec1490ba
caoshenkai <caoshenkai@neotel.com.cn>
2018-11-02 11:15:28 +0800

    更改util

--- client/src/main/scala/com/neo/sk/utils/EsheepClient.scala
+++ client/src/main/scala/com/neo/sk/utils/EsheepClient.scala
@@ -1,17 +1,15 @@
-package utils
+package com.neo.sk.utils
 
-import com.neo.sk.tank.common.AppSettings
 import com.neo.sk.tank.model._
-import com.neo.sk.tank.shared.ptcl.{ComRsp, ErrorRsp}
+import com.neo.sk.tank.shared.ptcl.ErrorRsp
 import org.slf4j.LoggerFactory
-
+import com.neo.sk.tank.App.{executor}
 import scala.concurrent.Future
-import com.neo.sk.tank.App.executor
+
 /**
   * Created by hongruying on 2018/10/16
   */
 object EsheepClient extends HttpUtil {
-  import io.circe._
   import io.circe.generic.auto._
   import io.circe.parser.decode
   import io.circe.syntax._
