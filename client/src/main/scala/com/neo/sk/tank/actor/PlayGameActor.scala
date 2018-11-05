e6ed42e26105aaa737978ad2fcb073b561a191a7
caoshenkai <caoshenkai@neotel.com.cn>
2018-11-05 11:09:39 +0800

    黑屏Debug

--- client/src/main/scala/com/neo/sk/tank/actor/PlayGameActor.scala
+++ client/src/main/scala/com/neo/sk/tank/actor/PlayGameActor.scala
@@ -205,6 +205,7 @@ object PlayGameActor {
     //todo 更改为目标端口
 //    val host = "10.1.29.250:30369"
 //    val host = info.gameInfo.domain
-    Route.getJoinGameWebSocketUri(info.playInfo.nickName,info.gameInfo.domain,info.playInfo,info.roomInfo)
+    Route.getJoinGameWebSocketUri(info.playInfo.nickName,"10.1.29.250"+":"+info.gameInfo.port,info.playInfo,info.roomInfo)
+//    s"ws://flowdev.neoap.com/tank/game/join&name=${info.playInfo.nickName}"
   }
 }
