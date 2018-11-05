82dcabb5b92125fe5afb8ced8833c6191bbbd4d3
wangmengyuan <879992488@qq.com>
2018-11-05 15:53:47 +0800

    死亡结算debug

--- client/src/main/scala/com/neo/sk/tank/actor/PlayGameActor.scala
+++ client/src/main/scala/com/neo/sk/tank/actor/PlayGameActor.scala
@@ -218,8 +218,8 @@ object PlayGameActor {
     * 链接由从平台获得IP和端口后拼接*/
   def getWebSocketUri(info:ConnectGame): String = {
     //todo 更改为目标端口
-    val host = "10.1.29.250:30369"
+//    val host = "10.1.29.250:30369"
 //    val host = info.gameInfo.domain
-    Route.getJoinGameWebSocketUri(info.playInfo.nickName,host,info.playInfo,info.roomInfo)
+    Route.getJoinGameWebSocketUri(info.playInfo.nickName,info.gameInfo.domain,info.playInfo,info.roomInfo)
   }
 }
