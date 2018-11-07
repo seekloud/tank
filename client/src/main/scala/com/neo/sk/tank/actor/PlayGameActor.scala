09292def5779580929af2169296a47b8edf0b1f1
wangmengyuan <879992488@qq.com>
2018-11-07 15:59:52 +0800

    client 增加死亡结算

--- client/src/main/scala/com/neo/sk/tank/actor/PlayGameActor.scala
+++ client/src/main/scala/com/neo/sk/tank/actor/PlayGameActor.scala
@@ -244,7 +244,7 @@ object PlayGameActor {
     //todo 更改为目标端口
     val host = "10.1.29.250:30369"
 //    val host = info.gameInfo.domain
-    Route.getUserJoinGameWebSocketUri(info.playInfo.nickName,info.gameInfo.domain,info.playInfo,info.roomInfo)
-    Route.getJoinGameWebSocketUri(info.playInfo.nickName,info.gameInfo.domain,info.roomInfo)
+    Route.getUserJoinGameWebSocketUri(info.playInfo.nickName,host,info.playInfo,info.roomInfo)
+    Route.getJoinGameWebSocketUri(info.playInfo.nickName,host,info.roomInfo)
   }
 }
