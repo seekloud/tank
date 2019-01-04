588fb5a38823b4a1a28f6a7f361fc660ca3b47af
benyafang <benyafang@neotel.com.cn>
2019-01-04 17:46:37 +0800

    merge

--- frontend/src/main/scala/com/neo/sk/tank/front/tankClient/control/GameHolder.scala
+++ frontend/src/main/scala/com/neo/sk/tank/front/tankClient/control/GameHolder.scala
@@ -174,9 +174,7 @@ abstract class GameHolder(name: String) extends NetworkInfo {
   }
 
   private def drawGame(offsetTime: Long,supportLiveLimit:Boolean = false) = {
-    gameContainerOpt.foreach(_.drawGame(offsetTime, getNetworkLatency,supportLiveLimit))
-  private def drawGame(offsetTime: Long) = {
-    gameContainerOpt.foreach(_.drawGame(offsetTime, getNetworkLatency, dateSize))
+    gameContainerOpt.foreach(_.drawGame(offsetTime, getNetworkLatency,dateSize,supportLiveLimit))
   }
 
 
