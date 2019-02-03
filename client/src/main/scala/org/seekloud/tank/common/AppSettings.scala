4b30f3f8f943ef3689ed175e76c6da4b390b6bae
hongruying <hongruying@neotel.com.cn>
2019-02-03 22:38:53 +0800

    copyright/ change package name

--- client/src/main/scala/org/seekloud/tank/common/AppSettings.scala
+++ client/src/main/scala/org/seekloud/tank/common/AppSettings.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 seekloud (https://github.com/seekloud)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.seekloud.tank.common
 
 import java.util.concurrent.TimeUnit
@@ -27,7 +43,17 @@ object AppSettings {
 
   val config = ConfigFactory.parseResources("product.conf").withFallback(ConfigFactory.load())
 
-
+  val appConfig = config.getConfig("app")
+  private val esheepConfig = appConfig.getConfig("esheep")
+  val esheepAppId = esheepConfig.getString("appId")
+  val esheepSecureKey = esheepConfig.getString("secureKey")
+  val esheepProtocol = esheepConfig.getString("protocol")
+  val esheepHost = esheepConfig.getString("host")
+  val esheepPort = esheepConfig.getInt("port")
+  val esheepDomain = esheepConfig.getString("domain")
+  val esheepGameId = esheepConfig.getLong("gameId")
+  val esheepGameKey = esheepConfig.getString("gsKey")
+  val esheepAuthToken = esheepConfig.getBoolean("authToken")
 
 
 
